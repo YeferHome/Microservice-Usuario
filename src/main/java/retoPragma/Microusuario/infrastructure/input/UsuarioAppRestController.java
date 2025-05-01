11f34f7391a59bd97c7d359f4023d2e0ce771a58
yeferHome <yefersonrojas16yi@gmai.com>
2025-05-01 18:10:38 -0500

    Actualizacion de EndPoints personalizados de guardados para cada Rol (Administrador,Propietario,Empleado)

--- src/main/java/retoPragma/Microusuario/infrastructure/input/UsuarioAppRestController.java
+++ src/main/java/retoPragma/Microusuario/infrastructure/input/UsuarioAppRestController.java
@@ -15,13 +15,13 @@ public class UsuarioAppRestController {
 
     private final IUsuarioAppHandler usuarioAppHandler;
 
-    @PostMapping("/saveAdministrador")
+    @PostMapping("/saveAdmin")
     public ResponseEntity<Void> saveUsuarioInUsuarioApp(@RequestBody UsuarioAppRequestDto usuarioAppRequestDto){
         usuarioAppHandler.saveUsuarioInUsuarioApp(usuarioAppRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/savePropietario")
+    @PostMapping("/saveOwner")
     public ResponseEntity<Void> savePropietarioInUsuarioApp(@RequestBody UsuarioAppRequestDto usuarioAppRequestDto){
         usuarioAppHandler.savePropietarioInUsuarioApp(usuarioAppRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -30,8 +30,7 @@ public class UsuarioAppRestController {
     @PostMapping("/saveEmpleado")
     public ResponseEntity<Void>saveEmpleadoInUsuarioApp (@RequestBody UsuarioAppRequestDto usuarioAppRequestDto){
         usuarioAppHandler.saveEmpleadoInUsuarioApp(usuarioAppRequestDto);
-    return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+    return ResponseEntity.status(HttpStatus.CREATED).build();}
 
     @GetMapping("/{id}/rol")
     String obtenerRolUsuario(@PathVariable("id") Long id){
